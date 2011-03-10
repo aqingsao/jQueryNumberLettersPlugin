51649511b4cc7650f7febfc7ff82b9485d2aacfb
vinci <vinci.zhang@gmail.com>
2011-03-10 23:10:28 +0800

    Could handle delete key

--- javascript/jquery.numberLetters.js
+++ javascript/jquery.numberLetters.js
@@ -24,11 +24,14 @@
     };
     $.fn.ip = function(options) {
 		var basic = "([1-9]|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])";
+		var empty = "^$";
 		var firstSection = basic;
 		var secondSection = basic + "\\." + basic;
 		var thirdSection = basic + "\\." + basic + "\\." + basic;
 		var fourthSection = basic + "\\." + basic + "\\." + basic + "\\." + basic;
-		var reg = new RegExp("^" + firstSection +"\\.?$|^" + secondSection +"\\.?$|^" + thirdSection + "\\.?$|^" + fourthSection + "$");
+		var reg = new RegExp(empty + "|^" + firstSection +"\\.?$|^" + secondSection +"\\.?$|^" + thirdSection + "\\.?$|^" + fourthSection + "$");
+		
+		var options = {_specialKeyRevalidated : [8]};//Delete key must be revalidated, as will might violate reg
 		return this.each(function(){checkByReg($(this), options, reg)});
     };
 
@@ -43,15 +46,24 @@
 	};
 
 	function checkByReg(input, options, reg){
+		var defaultOptions = {
+			_specialKeyRevalidated : []
+		};
+		var options = $.extend(defaultOptions, options);
 		input.keypress(function (e) {
             var key = e.which;
-            if (isSpecialKey(key) || (e.metaKey || e.ctrlKey)) {
+            if (e.metaKey || e.ctrlKey) {
                 return;
             }
+
+            if (isSpecialKey(key)){
+				return;
+            }
+
 			if(!reg.test(e.target.value)){
 				// Should handle this case
 			}
-			var newVal = getNewVal(e.target, String.fromCharCode(key));
+			var newVal = getNewVal(e.target, key);
 			if(reg.test(newVal)){
 				return;
 			}
@@ -63,7 +75,11 @@
         });
 	};
 	
-	function getNewVal(target, newChar){
+	function getNewVal(target, key){
+		if(key == 8){//delete key
+			return target.value.substring(0, target.selectionStart - 1) + target.value.substring(target.selectionEnd, target.textLength);
+		}
+		var newChar = String.fromCharCode(key);
 		if(target.selectionStart == target.selectionEnd && target.selectionStart == 0){
 				return newChar + target.value;
 	 	}
@@ -74,7 +90,7 @@
 			return target.value.substring(0, target.selectionStart) + newChar + target.value.substring(target.selectionEnd, target.textLength);
 		}
 	}
-
+	
     function isNumber(key) {
         return key >= 48 && key <= 57;
     }
@@ -84,7 +100,7 @@
     }
 
     function isSpecialKey(key) {
-        return key == 0 || key == 8 || key == 13 || key == 9;
+        return key == 0 || key == 13 || key == 9;
     }
 
 })(jQuery);
